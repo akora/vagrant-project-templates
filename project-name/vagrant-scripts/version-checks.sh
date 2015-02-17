58ca06685823f2196922e30094d0dad5e5fe58b5
Andras Kora <ak@akora.info>
2015-02-17 19:32:51 +0100

    implemented proper check for package existence before reporting version

--- project-name/vagrant-scripts/version-checks.sh
+++ project-name/vagrant-scripts/version-checks.sh
@@ -8,24 +8,29 @@ print_header "Version checks"
 packages=(python git node npm bower)
 
 for package_to_check in ${packages[@]}; do
-  if [[ "$package_to_check" = "python" ]]; then
-    version="$(echo `python -c 'import sys; print(sys.version[:5])'`)"
-    print_message_with_param $package_to_check $version
-    indicator_green
-  elif [[ "$package_to_check" = "git" ]]; then
-    version="$(echo `git --version` | awk '{print $3}')"
-    print_message_with_param $package_to_check $version
-    indicator_green
-  else
-    version="$(echo `$package_to_check -v`)"
-    if [[ "$version" =~ "v" ]]; then
-      version="$(echo $version | cut -c2-)"
+  if command -v $package_to_check >/dev/null 2>&1; then
+    if [[ "$package_to_check" = "python" ]]; then
+      version="$(echo `python -c 'import sys; print(sys.version[:5])'`)"
+      print_message_with_param $package_to_check $version
+      indicator_green
+    elif [[ "$package_to_check" = "git" ]]; then
+      version="$(echo `git --version` | awk '{print $3}')"
       print_message_with_param $package_to_check $version
       indicator_green
     else
-    print_message_with_param $package_to_check $version
-    indicator_green
+      version="$(echo `$package_to_check -v`)"
+      if [[ "$version" =~ "v" ]]; then
+        version="$(echo $version | cut -c2-)"
+        print_message_with_param $package_to_check $version
+        indicator_green
+      else
+        print_message_with_param $package_to_check $version
+        indicator_green
+      fi
     fi
+  else
+    print_message_with_param "$package_to_check is not installed" "-----"
+    indicator_amber
   fi
 done
 
